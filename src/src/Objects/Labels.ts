02d18c42704fb2c94369f9f650d93a2f75b65290
Marc Dittmann <Marcd@Dittmann-media.de>
2017-05-30 18:17:34 +0200

    no message

---
+++ src/src/Objects/Labels.ts
@@ -0,0 +1,13 @@
+
+
+import {GridscaleObjects} from './GridscaleObjects';
+
+
+class Labels extends GridscaleObjects {
+
+    constructor(_api) { super(_api,'/objects/labels'); }
+
+}
+
+
+export { Labels }
