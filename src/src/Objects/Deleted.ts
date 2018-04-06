e6babcaaeb1dc5b842f14f48a318b0e58b1dcf19
Marc Dittmann <Marcd@Dittmann-media.de>
2018-04-06 14:31:11 +0200

    Add Deleted

--- src/src/Objects/Deleted.ts
+++ src/src/Objects/Deleted.ts
@@ -1,8 +1,9 @@
-
+import * as _ from "lodash";
 class Deleted {
 
     // Naming
     public _api;
+    public _defaults;
 
 
     /**
@@ -14,10 +15,75 @@ class Deleted {
      */
     constructor(_api){
         this._api = _api;
+
+        this._defaults = {
+            "page": 0,
+            "limit" : 25
+        }
     }
 
-    ips( _options,_callback? ) {
-        return this._api.get( '/objects/deleted/ips',_options,_callback)
+    /**
+     * Overwrite Default Settings for this Type
+     *
+     * @param _options
+     */
+    public setDefaults( _options ) {
+
+         _.assignIn( this._defaults , _options );
+    }
+
+    _buildRequestOptions (_options?) {
+
+        // Clone Defaults
+        var defaults = _.assignIn({},this._defaults);
+
+        // Add Options
+        if ( !_.isUndefined( _options ) && !_.isFunction( _options ) ) {
+            _.assignIn(defaults,_options);
+        }
+
+        // Return Default Values
+        return defaults;
+    }
+
+    _deleted(_key, _options?,_callback?){
+      var requestOptions = this._buildRequestOptions(_options);
+
+      // Set Callback
+      if ( _.isFunction( _options ) && _.isUndefined( _callback ) ) {
+          _callback = _options
+      }
+        return this._api.get( '/objects/deleted/'+_key,_options,_callback)
+    }
+
+
+    ips( _options?,_callback? ) {
+      return this._deleted('ips',_options,_callback);
+    }
+
+
+    isoimages( _options?,_callback? ) {
+      return this._deleted('isoimages',_options,_callback);
+    }
+
+    networks( _options?,_callback? ) {
+      return this._deleted('networks',_options,_callback);
+    }
+
+    servers( _options?,_callback? ) {
+      return this._deleted('servers',_options,_callback);
+    }
+
+    snapshots( _options?,_callback? ) {
+      return this._deleted('snapshots',_options,_callback);
+    }
+
+    storages( _options?,_callback? ) {
+      return this._deleted('storages',_options,_callback);
+    }
+
+    templates( _options?,_callback? ) {
+      return this._deleted('templates',_options,_callback);
     }
 
 
