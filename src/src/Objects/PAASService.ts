acc44617b7364e718f8d545d4cc7a561c7676fcf
Jan Stuhlmann <jan@gridscale.io>
2019-12-04 14:19:06 +0100

    add renewCredentials

--- src/src/Objects/PAASService.ts
+++ src/src/Objects/PAASService.ts
@@ -7,7 +7,9 @@ class PAASService extends GridscaleObjects {
 
     constructor(_api) { super(_api,'/objects/paas/services'); }
 
-
+    renewCredentials(_serviceUUID) {
+        return this._api.patch(this._basepath + '/' + _serviceUUID + '/renew_credentials', {});
+    }
 }
 
 
