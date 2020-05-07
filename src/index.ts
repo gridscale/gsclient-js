8f613fd5465683057a53351992ef48f19b6fe997
Jan Stuhlmann <jan@gridscale.io>
2020-05-07 15:56:25 +0200

    export models

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 /**
  * Export all publicly accessible modules
  */
-import * as gridscale from "./src/client";
+import * as gridscale from './src/client';
 export {gridscale};
+
+export * from './src/Objects/model/models';
