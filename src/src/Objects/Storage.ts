289b9aa0d45285ce5a7547984e97fa3b51a69f1a
Jan Stuhlmann <jan@gridscale.io>
2020-06-18 16:28:07 +0200

    fix path

--- src/src/Objects/Storage.ts
+++ src/src/Objects/Storage.ts
@@ -303,7 +303,7 @@ class Storage extends GridscaleObjects {
      * @param _callback
      */
     createFromBackup(_name: string, _backup_uuid: string, _callback?: Function): Promise<ApiResult<VoidApiResult>> {
-        return this._api.post(this._basepath + '/import/', {
+        return this._api.post(this._basepath + '/import', {
             name: _name,
             backup_uuid: _backup_uuid
         }, _callback);
