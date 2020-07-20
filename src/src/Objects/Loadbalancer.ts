9682ef8ebe9cb1da159077f0a7a0ba040225aa48
Jan Stuhlmann <jan@gridscale.io>
2020-07-20 16:09:54 +0200

    loadbalancer response interfaces

--- src/src/Objects/Loadbalancer.ts
+++ src/src/Objects/Loadbalancer.ts
@@ -1,13 +1,30 @@
 
 
 import {GridscaleObjects} from './GridscaleObjects';
-import { APIClass } from '../api';
+import { APIClass, RequestOptions, ApiResult } from '../api';
+import * as models from './model/models';
 
 
 class Loadbalancer extends GridscaleObjects {
 
     constructor(_api: APIClass) { super(_api, '/objects/loadbalancers'); }
-
+    
+    // override some generic function to set explicit return type
+    list(_options?: RequestOptions, _callback?: Function): Promise<ApiResult<models.LoadbalancersGetResponse>> {
+        return super._pipe_result(
+            super.list(_options, _callback),
+            'loadbalancers'
+        );
+    }
+    get(_uuid: string, _callback?: Function): Promise<ApiResult<models.LoadbalancerGetResponse>> {
+        return super._pipe_result(
+            super.get(_uuid, _callback),
+            'loadbalancer'
+        );
+    }
+    create(_attributes: Object, _callback?: Function): Promise<ApiResult<models.CreateResponse>> {
+        return super.create(_attributes, _callback);
+    }
 }
 
 export { Loadbalancer };
