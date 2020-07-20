bec8af48f91f2574e0d8b4f68470b62f87bb2778
Jan Stuhlmann <jan@gridscale.io>
2020-07-20 16:47:54 +0200

    marketplace response interface

--- src/src/Objects/Marketplace.ts
+++ src/src/Objects/Marketplace.ts
@@ -1,5 +1,5 @@
 import {GridscaleObjects} from './GridscaleObjects';
-import { APIClass } from '../api';
+import { APIClass, ApiResult, RequestOptions } from '../api';
 import * as models from './model/models';
 
 
@@ -7,7 +7,22 @@ class Marketplace extends GridscaleObjects {
 
     constructor(_api: APIClass) { super(_api, '/objects/marketplace/applications'); }
 
-
+    // override some generic function to set explicit return type
+    list(_options?: RequestOptions, _callback?: Function): Promise<ApiResult<models.MarketplaceApplicationsGetResponse>> {
+        return super._pipe_result(
+            super.list(_options, _callback),
+            'applications'
+        );
+    }
+    get(_uuid: string, _callback?: Function): Promise<ApiResult<models.MarketplaceApplicationGetResponse>> {
+        return super._pipe_result(
+            super.get(_uuid, _callback),
+            'application'
+        );
+    }
+    create(_attributes: Object, _callback?: Function): Promise<ApiResult<models.MarketplaceApplicationCreateResponse>> {
+        return super.create(_attributes, _callback);
+    }
 }
 
 
