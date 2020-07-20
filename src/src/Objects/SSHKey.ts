ad781dd730a3ba1fc9a19a30841f538c9ee1b852
Jan Stuhlmann <jan@gridscale.io>
2020-07-20 17:10:18 +0200

    sshkey response interface

--- src/src/Objects/SSHKey.ts
+++ src/src/Objects/SSHKey.ts
@@ -1,13 +1,31 @@
 
 
 import {GridscaleObjects} from './GridscaleObjects';
-import { APIClass } from '../api';
+import { APIClass, RequestOptions, ApiResult } from '../api';
+import * as models from './model/models';
 
 
 class SSHKey extends GridscaleObjects {
 
     constructor(_api: APIClass) { super(_api, '/objects/sshkeys'); }
-
+    
+    
+    // override some generic function to set explicit return type
+    list(_options?: RequestOptions, _callback?: Function): Promise<ApiResult<models.SshkeysGetResponse>> {
+        return super._pipe_result(
+            super.list(_options, _callback),
+            'sshkeys'
+        );
+    }
+    get(_uuid: string, _callback?: Function): Promise<ApiResult<models.SshkeyGetResponse>> {
+        return super._pipe_result(
+            super.get(_uuid, _callback),
+            'sshkey'
+        );
+    }
+    create(_attributes: Object, _callback?: Function): Promise<ApiResult<models.CreateResponse>> {
+        return super.create(_attributes, _callback);
+    }
 }
 
 export { SSHKey };
