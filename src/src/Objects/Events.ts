cf65db30ebe271e5d7027f356b7238c4d671012a
Jan Stuhlmann <jan@gridscale.io>
2020-07-20 14:18:24 +0200

    event response interface

--- src/src/Objects/Events.ts
+++ src/src/Objects/Events.ts
@@ -1,13 +1,20 @@
 
 
 import {GridscaleObjects} from './GridscaleObjects';
-import { APIClass } from '../api';
+import { APIClass, RequestOptions, ApiResult } from '../api';
+import { EventResponse } from './model/models';
 
 
 class Events extends GridscaleObjects {
 
     constructor(_api: APIClass) { super(_api, '/objects/events'); }
-
+    
+    list(_options?: RequestOptions, _callback?: Function): Promise<ApiResult<EventResponse>> {
+        return super._pipe_result(
+            super.list(_options, _callback),
+            'events'
+        );
+    }
 }
 
 
