6044301b49ba842b94e763ed944e3b2436c8fb9b
Jan Stuhlmann <jan@gridscale.io>
2020-07-20 16:04:10 +0200

    ip response interfaces

--- src/src/Objects/IP.ts
+++ src/src/Objects/IP.ts
@@ -2,6 +2,7 @@
 
 import { GridscaleObjects } from './GridscaleObjects';
 import { APIClass, ApiResult, GSError, GenericApiResult, RequestOptions } from '../api';
+import * as models from './model/models';
 
 
 class IP extends GridscaleObjects {
@@ -17,8 +18,24 @@ class IP extends GridscaleObjects {
      * @param _callback
      * @returns {any}
      */
-    list(_options?: RequestOptions, _callback?: Function): Promise<ApiResult<GenericApiResult>> {
-        return this._api.get( this._basepath , _options, _callback);
+    list(_options?: RequestOptions, _callback?: Function): Promise<ApiResult<models.IpsGetResponse>> {
+        return this._pipe_result(
+            this._api.get( this._basepath , _options, _callback),
+            'ips'
+        );
+
+    }
+
+    // override some generic function to set explicit return type
+    
+    get(_uuid: string, _callback?: Function): Promise<ApiResult<models.IpGetResponse>> {
+        return super._pipe_result(
+            super.get(_uuid, _callback),
+            'ip'
+        );
+    }
+    create(_attributes: Object, _callback?: Function): Promise<ApiResult<models.IpCreateResponse>> {
+        return super.create(_attributes, _callback);
     }
 
 }
