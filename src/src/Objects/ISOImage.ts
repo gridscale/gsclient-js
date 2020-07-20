c4f2c478fa6cef82553103901e761efc8f074c90
Jan Stuhlmann <jan@gridscale.io>
2020-07-20 16:06:22 +0200

    iso images2 response interfaces

--- src/src/Objects/ISOImage.ts
+++ src/src/Objects/ISOImage.ts
@@ -1,11 +1,31 @@
 
 
 import {GridscaleObjects} from './GridscaleObjects';
-import { APIClass } from '../api';
+import { APIClass, RequestOptions, ApiResult } from '../api';
+import * as models from './model/models';
 
 
 class ISOImage extends GridscaleObjects {
     constructor(_api: APIClass) { super(_api, '/objects/isoimages'); }
+
+    // override some generic function to set explicit return type
+    list(_options?: RequestOptions, _callback?: Function): Promise<ApiResult<models.IsoimagesGetResponse>> {
+        return super._pipe_result(
+            super.list(_options, _callback),
+            'isoimages'
+        );
+    }
+    get(_uuid: string, _callback?: Function): Promise<ApiResult<models.IsoimageGetResponse>> {
+        return super._pipe_result(
+            super.get(_uuid, _callback),
+            'isoimage'
+        );
+    }
+    create(_attributes: Object, _callback?: Function): Promise<ApiResult<models.CreateResponse>> {
+        return super.create(_attributes, _callback);
+    }
+
+
 }
 
  export { ISOImage };
