dae00639a6836a8d3befab03560819d91140f9bd
Jan Stuhlmann <jan@gridscale.io>
2020-07-20 17:20:12 +0200

    template response interface

--- src/src/Objects/Template.ts
+++ src/src/Objects/Template.ts
@@ -1,13 +1,30 @@
 
 
 import {GridscaleObjects} from './GridscaleObjects';
-import { APIClass } from '../api';
+import { APIClass, RequestOptions, ApiResult } from '../api';
+import * as models from './model/models';
 
 
 class Template extends GridscaleObjects {
 
     constructor(_api: APIClass) { super(_api, '/objects/templates'); }
-
+    
+    // override some generic function to set explicit return type
+    list(_options?: RequestOptions, _callback?: Function): Promise<ApiResult<models.StorageTemplatesGetResponse>> {
+        return super._pipe_result(
+            super.list(_options, _callback),
+            'templates'
+        );
+    }
+    get(_uuid: string, _callback?: Function): Promise<ApiResult<models.TemplateGetResponse>> {
+        return super._pipe_result(
+            super.get(_uuid, _callback),
+            'template'
+        );
+    }
+    create(_attributes: Object, _callback?: Function): Promise<ApiResult<models.CreateResponse>> {
+        return super.create(_attributes, _callback);
+    }
 }
 
 
