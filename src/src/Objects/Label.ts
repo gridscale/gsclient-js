869674b5e064e57fd68ee7f623c00300ba4ad7ff
Jan Stuhlmann <jan@gridscale.io>
2020-07-20 16:08:07 +0200

    label response interfaces

--- src/src/Objects/Label.ts
+++ src/src/Objects/Label.ts
@@ -1,10 +1,24 @@
 import {GridscaleObjects} from './GridscaleObjects';
-import { APIClass } from '../api';
+import { APIClass, RequestOptions, ApiResult } from '../api';
+import * as models from './model/models';
 
 
 class Label extends GridscaleObjects {
 
     constructor(_api: APIClass) { super(_api, '/objects/labels'); }
+    // override some generic function to set explicit return type
+    list(_options?: RequestOptions, _callback?: Function): Promise<ApiResult<models.LabelsGetResponse>> {
+        return super._pipe_result(
+            super.list(_options, _callback),
+            'labels'
+        );
+    }
+    get(_uuid: string, _callback?: Function): Promise<ApiResult<models.LabelGetResponse>> {
+        return super._pipe_result(
+            super.get(_uuid, _callback),
+            'label'
+        );
+    }
 
 
 }
