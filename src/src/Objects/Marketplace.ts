364cd2a0fdc6366266bb89ae8bd597bfc5899935
Jan Stuhlmann <jan@gridscale.io>
2020-07-20 16:32:13 +0200

    network response interface

--- src/src/Objects/Marketplace.ts
+++ src/src/Objects/Marketplace.ts
@@ -1,5 +1,6 @@
 import {GridscaleObjects} from './GridscaleObjects';
 import { APIClass } from '../api';
+import * as models from './model/models';
 
 
 class Marketplace extends GridscaleObjects {
--- src/src/Objects/Network.ts
+++ src/src/Objects/Network.ts
@@ -1,13 +1,30 @@
 
 
 import {GridscaleObjects} from './GridscaleObjects';
-import { APIClass } from '../api';
+import { APIClass, RequestOptions, ApiResult } from '../api';
+import * as models from './model/models';
 
 
 class Network extends GridscaleObjects {
 
     constructor(_api: APIClass) { super(_api, '/objects/networks'); }
-
+    
+    // override some generic function to set explicit return type
+    list(_options?: RequestOptions, _callback?: Function): Promise<ApiResult<models.NetworksGetResponse>> {
+        return super._pipe_result(
+            super.list(_options, _callback),
+            'networks'
+        );
+    }
+    get(_uuid: string, _callback?: Function): Promise<ApiResult<models.NetworkGetResponse>> {
+        return super._pipe_result(
+            super.get(_uuid, _callback),
+            'network'
+        );
+    }
+    create(_attributes: Object, _callback?: Function): Promise<ApiResult<models.CreateResponse>> {
+        return super.create(_attributes, _callback);
+    }
 }
 
 
