f9df8d415637bd339c778d2f70d8f744796e292c
Jan Stuhlmann <jan@gridscale.io>
2021-01-22 17:12:19 +0100

    comment

--- scripts/types_from_apispec.js
+++ scripts/types_from_apispec.js
@@ -16,6 +16,12 @@ const specFile = __dirname + '/spec.yaml';
 const specUrl = "https://ce85b99cc46752fffee35cab9a7b0278abb4c2d2055cff685af4912c49490f8.gos3.io/spec.yaml";
 const distDir = __dirname + '/../src/Specs';
 
+
+/**
+ * convert "import|export type { ... } from ..." to 
+ *         "import|export { ... } from ..." to be compatible to Typescript < 3.8
+ * @param {*} distDir 
+ */
 const removeTypeImExportRecursive = (distDir) => {
     fs.readdir(distDir, (err, files) => {
         if (err) {
