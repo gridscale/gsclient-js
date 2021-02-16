ab1b104b57604c8f5c94ff20ac7b58e55d44088b
Jan Stuhlmann <jan@gridscale.io>
2021-02-16 12:00:59 +0100

    allow name patch for private template

--- src/Objects/Template.ts
+++ src/Objects/Template.ts
@@ -1,7 +1,7 @@
 
 
 import {GridscaleObjects} from './GridscaleObjects';
-import { APIClass, RequestOptions, ApiResult } from '../api';
+import { APIClass, RequestOptions, ApiResult, GenericApiResult, VoidApiResult } from '../api';
 import * as models from './../Specs';
 import { StorageTemplateCreate } from './../Specs';
 
@@ -9,6 +9,7 @@ interface Template {
     list(_options?: RequestOptions, _callback?: Function): Promise<ApiResult<models.StorageTemplatesGetResponse>>;
     get(_uuid: string, _callback?: Function): Promise<ApiResult<models.TemplateGetResponse>>;
     create(_attributes: StorageTemplateCreate, _callback?: Function): Promise<ApiResult<models.CreateResponse>>;
+    patch(_uuid: string, _attributes: { name: string }, _callback?: (response: Response, result: ApiResult<GenericApiResult>) => void): Promise<ApiResult<VoidApiResult>>
 }
 
 class Template extends GridscaleObjects {
