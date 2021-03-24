1de8043c391e2e70580ac952aa8d12826f007ac3
Marc Dittmann <Marc@Dittmann-Media.de>
2021-03-24 10:26:06 +0100

    Add Certificate (List/Get/Create) and Update Sepcs

---
+++ src/Objects/Certificate.ts
@@ -0,0 +1,27 @@
+import { GridscaleObjects } from "./GridscaleObjects";
+import { APIClass, ApiResult, RequestOptions } from "../api";
+import * as models from "./../Specs";
+import { CertificateCreate } from "./../Specs";
+
+interface Certificate {
+  list(
+    _options?: RequestOptions,
+    _callback?: Function
+  ): Promise<Omit<ApiResult<models.CertificatesGetResponse>, "meta" | "links">>;
+  get(
+    _uuid: string,
+    _callback?: Function
+  ): Promise<ApiResult<models.CertificateGetResponse>>;
+  create(
+    _attributes: CertificateCreate,
+    _callback?: Function
+  ): Promise<ApiResult<models.CreateResponse>>;
+}
+
+class Certificate extends GridscaleObjects {
+  constructor(_api: APIClass) {
+    super(_api, "/objects/certificates");
+  }
+}
+
+export { Certificate };
--- src/Specs/models/LoadbalancerUpdate.ts
+++ src/Specs/models/LoadbalancerUpdate.ts
@@ -8,27 +8,27 @@ export type LoadbalancerUpdate = {
      */
     name?: string;
     /**
-     * An array of objects containing the forwarding rules for the Load balancer (
-         */
-        forwarding_rules?: Array<any>;
-        /**
-         * The servers that this Load balancer can communicate with
-         */
-        backend_servers?: Array<any>;
-        /**
-         * Status indicates the status of the object.
-         */
-        status?: string;
-        /**
-         * Whether the Load balancer is forced to redirect requests from HTTP to HTTPS.
-         */
-        redirect_http_to_https?: boolean;
-        /**
-         * List of labels.
-         */
-        labels?: Array<string>;
-        /**
-         * The algorithm used to process requests. Accepted values: roundrobin / leastconn.
-         */
-        algorithm?: string;
-    }
+     * An array of objects containing the forwarding rules of the load balancer. The listen_port field specifies the entry port of the load balancer and the target_port field specifies the exit port that the load balancer uses to forward the traffic to the backend server. The load balancer supports HTTP and TCP modes. Furthermore, the load balancer supports SSL termination for letsencrypt and custom certificates (e.g., bring your own certificate). The certificate_uuid is the UUID of a custom certificate and is an optional field, but letsencrypt is a required field, which should be set to null if no letsencrypt is requested.
+     */
+    forwarding_rules?: Array<any>;
+    /**
+     * The servers that this Load balancer can communicate with
+     */
+    backend_servers?: Array<any>;
+    /**
+     * Status indicates the status of the object.
+     */
+    status?: string;
+    /**
+     * Whether the Load balancer is forced to redirect requests from HTTP to HTTPS.
+     */
+    redirect_http_to_https?: boolean;
+    /**
+     * List of labels.
+     */
+    labels?: Array<string>;
+    /**
+     * The algorithm used to process requests. Accepted values: roundrobin / leastconn.
+     */
+    algorithm?: string;
+}
