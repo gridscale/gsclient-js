4c44b0f21d2d0249cc24498d2117c2ed95b15223
Jan Stuhlmann <jan@gridscale.io>
2021-08-18 16:18:56 +0200

    pin server to dhcp ip

--- src/Objects/Network.ts
+++ src/Objects/Network.ts
@@ -1,7 +1,7 @@
 
 
 import {GridscaleObjects} from './GridscaleObjects';
-import { APIClass, RequestOptions, ApiResult, VoidApiResult } from '../api';
+import { APIClass, RequestOptions, ApiResult, VoidApiResult, GenericApiResult } from '../api';
 import * as models from './../Specs';
 import { NetworkCreate, NetworkUpdate } from './../Specs';
 
@@ -15,6 +15,43 @@ interface Network {
 class Network extends GridscaleObjects {
 
     constructor(_api: APIClass) { super(_api, '/objects/networks'); }
+
+    /**
+     * List pinned servers
+     *
+     * @param _network_uuid
+     * @param _callback
+     * @returns {Promise<ApiResult<VoidApiResult>>}
+     */
+    pinnedServers(_network_uuid: string, _callback?: (response: Response, result: ApiResult<GenericApiResult>) => void): Promise<ApiResult<models.NetworkPinnedServersResponse>> {
+        return this._api.get(  this._basepath + '/' + _network_uuid + '/pinned_servers', _callback );
+    }
+
+    /**
+     * Pin a server to an ip
+     *
+     * @param _network_uuid
+     * @param _server_uuid
+     * @param _ip
+     * @param _callback
+     * @returns {Promise<ApiResult<VoidApiResult>>}
+     */
+    pinServerIp(_network_uuid: string, _server_uuid: string, _payload: models.PinnedServerPayload, _callback?: (response: Response, result: ApiResult<GenericApiResult>) => void): Promise<ApiResult<VoidApiResult>> {
+        return this._api.patch(  this._basepath + '/' + _network_uuid + '/pinned_servers/' + _server_uuid , _payload , _callback );
+    }
+
+    /**
+     * Unpin a server from an ip
+     *
+     * @param _network_uuid
+     * @param _server_uuid
+     * @param _ip
+     * @param _callback
+     * @returns {Promise<ApiResult<VoidApiResult>>}
+     */
+    unpinServerIp(_network_uuid: string, _server_uuid: string, _callback?: (response: Response, result: ApiResult<GenericApiResult>) => void): Promise<ApiResult<VoidApiResult>> {
+        return this._api.remove(  this._basepath + '/' + _network_uuid + '/pinned_servers/' + _server_uuid , _callback );
+    }
 }
 
 
