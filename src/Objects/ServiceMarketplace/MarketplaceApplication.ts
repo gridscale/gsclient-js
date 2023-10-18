4631e660a60cb8edb18fd6ef2a838131db229fda
Jan Stuhlmann <jan@gridscale.io>
2023-10-18 07:39:14 +0200

    new catalog endpoints for service marketplace

--- src/Objects/ServiceMarketplace/MarketplaceApplication.ts
+++ src/Objects/ServiceMarketplace/MarketplaceApplication.ts
@@ -18,6 +18,14 @@ class MarketplaceApplication extends GridscaleObjects {
   constructor(_api: APIClass) {
     super(_api, '/marketplace/v1/applications');
   }
+
+  catalog(_options?: RequestOptions, _callback?: (response: Response, result: ApiResult<MarketplaceApplicationListResponse>) => void): Promise<ApiResult<MarketplaceApplicationListResponse>> {
+    return this._api.get(this._basepath, _options, _callback);
+  }
+
+  catalogForAccount(account_uuid: string, _options?: RequestOptions, _callback?: (response: Response, result: ApiResult<MarketplaceApplicationListResponse>) => void): Promise<ApiResult<MarketplaceApplicationListResponse>> {
+    return this._api.get(this._basepath + '/' + account_uuid, _options, _callback);
+  }
 }
 
 export { MarketplaceApplication };
